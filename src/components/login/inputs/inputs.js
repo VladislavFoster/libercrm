1c9b14b9ae8a17e496355cef258d92fabdd373f9
EikoFoster <arctic.fox.coder@gmail.com>
2022-12-12 05:21:22 +0300

    zdfb

--- src/components/login/inputs/inputs.js
+++ src/components/login/inputs/inputs.js
@@ -1,18 +1,37 @@
 import React, {useState} from 'react'
 import style from '../../../layout/styles/auth.module.css';
 
+function handleLogin(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "http://libercrm", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+
+    if(xhttp.readyState === 4 && xhttp.status === 200){
+      window.location.href = '/rental'
+    }else{  
+      console.log('Ошибка')
+    }
+
+}
+
 function Inputs() {
     const [userName, setUserName] = useState()
     const [userPassword, setUserPassword] = useState()
     
     return (
-        
+        <div className={style['authBox']}>
+              <h1 className={style['heading']}>Авторизация</h1>
               <div className={style['inputs']}>
                     <h5 className={style['heading']}>Логин</h5>
+                 
                     <input type="text" onChange={(event)=> setUserName(event.target.value)} value={userName}/>
                     <h5 className={style['heading']}>Пароль</h5>
+
                     <input type="password" onChange={(event)=> setUserPassword(event.target.value)} value={userPassword}/>
                 </div>
+                <button onClick={handleLogin}><h5>Войти</h5></button> 
+          </div>
               
     )
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,46 +1,26 @@
-import React, {useState, useMemo, useCallback, useRef} from 'react';
+import React, {useState,  useRef} from 'react';
 import bg from '../../assets/imgs/bgAuthImg.png';
 import style from '../../layout/styles/auth.module.css';
 import Inputs from './inputs/inputs';
 
-function createUser(name, surName){
-  return{
-    name, surName
-  }
-}
-
 
 const Login = () => {
 
-
+  
 
   // const generateUser = useMemo(()=>{createUser(userName, userSurname)}, [userName, userSurname])
   // console.log(generateUser)
 
-  const handlerLogin = useCallback(()=>{
-    const xhttp = new XMLHttpRequest();
 
-    xhttp.open("POST", "http://libercrm", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    
-
-    if(xhttp.readyState == 4 && xhttp.status == 200){
-      window.location.href = '/rental'
-    }else{  
-      console.log('Ошибка')
-    }
-  })
 
   
 
   return (
     
       <div className={style['main']}>
-          <div className={style['authBox']}>
-              <h1 className={style['heading']}>Авторизация</h1>
+          
               <Inputs/>
-            <button onClick={handlerLogin()}><h5>Войти</h5></button> 
-          </div>
+            
           <img src={bg} alt="" />
       </div>
 
