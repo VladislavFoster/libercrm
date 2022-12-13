c9edbc573bf2a56c7e187393493d3d4d3b7164ff
EikoFoster <arctic.fox.coder@gmail.com>
2022-12-13 20:31:12 +0300

    грло

--- src/components/rental/rental.js
+++ src/components/rental/rental.js
@@ -3,14 +3,57 @@ import Header from '../../screens/header';
 import Nav from '../../screens/nav';
 import globalStyle from '../../layout/styles/globalStyle.module.css';
 import style from '../../layout/styles/rental.module.css'
+import arrowNavImg from '../../assets/imgs/arrowNavImg.png'
 
 const Rental = () => {
   return (
     <div>
       <Header/>
       <Nav/>
-      <div className={globalStyle['main']}>
-        <h1>test</h1>
+      <div className={`${globalStyle['main']} ${style['main']}`}>
+        <div className={style['infoContainer']}>
+          <div className={style['infoDescr']}>
+            <div className={style['item']}>
+              <h5><span>ОТВЕТСТВЕННЫЙ</span></h5>
+              <img src={arrowNavImg} alt="" />
+            </div>
+            <div className={style['item']}>
+              <h5><span>КЛИЕНТ</span></h5>
+              <img src={arrowNavImg} alt="" />
+            </div>
+            <div className={style['item']}>
+              <h5><span>КНИГА</span></h5>
+              <img src={arrowNavImg} alt="" />
+            </div>
+            <div className={style['item']}>
+              <h5><span>ДАТА ПОЛУЧЕНИЯ/СДАЧИ</span></h5>
+              <img src={arrowNavImg} alt="" />
+            </div>
+            <div className={style['item']}>
+              <h5><span>СТАТУС</span></h5>
+              <img src={arrowNavImg} alt="" />
+            </div>
+          </div>
+          <div className={style['infoItems']}>
+            <div className={style['container']}>
+              <div className={style['item']}>
+                <h5>ФИО сотрудника</h5>
+              </div>
+              <div className={style['item']}>
+                <h5>ФИО клента</h5>
+              </div>
+              <div className={style['item']}>
+                <h5>Название книги</h5>
+              </div>
+              <div className={style['item']}>
+                <h5>123стр</h5>
+              </div>
+              <div className={style['item']}>
+                <div className={style['green']}><h5>Недавно выдано</h5></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
